5c0cb1690abb44998dd5f32dde32f49aa687a4b6
Erika <adob.e@hotmail.com>
2022-07-01 22:44:50 +0200

    lot of changes

--- game.js
+++ game.js
@@ -3,33 +3,82 @@ const ocean         = ['Hyppocampe', 'Hyppocampe', 'Poisson Clown','Poisson Clow
 const savane        = ['Lion', 'Lion', 'Girafe', 'Girafe', 'Zèbre', 'Zèbre', 'Éléphant', 'Éléphant', 'Chimpanzé', 'Chimpanzé', 'Hippopotame', 'Hippopotame'];
 const ferme         = ['Vache', 'Vache', 'Poule', 'Poule', 'Chien', 'Chien', 'Mouton', 'Mouton', 'Cochon', 'Cochon', 'Cheval', 'Cheval']
 const themes        = [ocean, savane, ferme];
-//Ce que je dois faire : 
-//_____1_____
-//Un event listener sur les cards. Quand ça clique : 
-//Retourne la carte 
 
-//Distribution des cartes au tout début !
+let listCards       = [];
+let counter       = 0;
+//____________1__________Distribution des cartes au tout début :
 CardsDistribution();
+
+//____________2__________Cartes qui se retournent :
 cards.forEach(card => {
     card.addEventListener('click', () => {
         //  Retourne la carte
         let front = card.querySelector('.front');
         let back  = card.querySelector('.back');
+
         front.classList.add('flipper-front-on');
         back.classList.add('flipper-back-on');
+        //Verification des cartes:
+        //Récupérer le nom de la data
+        VerifierCartes(card);
     })
 })
+function VerifierCartes(card){
+    listCards.push(card);
+    listCards[0].dataset.number = "one";
+    console.log(listCards)
+    if (listCards.length == 2) {
+        //  Si on appuie deux fois sur la même carte :
+        if (listCards[1].dataset.number == "one") {
+            listCards.pop();
+        } else {
+            let firstCard  = listCards[0];
+            let secondCard = listCards[1];
+            if (firstCard.dataset.animal === secondCard.dataset.animal) {
+                console.log("je passe ici")
+                //Carte en vert
+                //Remettre vide listCards
+            } else {
+                //Carte en rouge, petite animation de droite à gauche
+                //Retourner les deux cartes 
+                console.log("Je passe ici2")
+                listCards.forEach(card => {
+                    let front = card.querySelector('.front');
+                    let back  = card.querySelector('.back');
+                    setTimeout(TurnCards, 1000,front, back);
+                })
+            listCards = [];
+        }
+        console.log("Après le retournement" + listCards);
+        }
+    }
+}
+
+function TurnCards(front, back){
+    front.classList.remove('flipper-front-on');
+    back.classList.remove('flipper-back-on');
+    front.classList.add('flipper-front-off');
+    back.classList.add('flipper-back-off');
+}
+
+function getFrontAndBack(card){
+    let front = card.querySelector('.front');
+    let back  = card.querySelector('.back');
+}
+
 function SelectTheme() {
     return Math.floor(Math.random() * (themes.length));
 }
+
 function CardsDistribution() {
     let theme = SelectTheme();
     theme     = themes[theme];
     let cloneTheme   = [...theme];
     cards.forEach(card => {
-        let index        = Math.floor(Math.random() * (cloneTheme.length));
-        let back         = card.querySelector('.back');
-        back.innerText   = cloneTheme[index];
+        let index           = Math.floor(Math.random() * (cloneTheme.length));
+        let back            = card.querySelector('.back');
+        back.innerText      = cloneTheme[index];
+        card.dataset.animal = cloneTheme[index];
         cloneTheme.splice(index, 1);
     })
 }
