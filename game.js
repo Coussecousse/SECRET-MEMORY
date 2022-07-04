263c712296d68484f5eff2a95467fa772346dc7e
Erika <adob.e@hotmail.com>
2022-07-04 18:23:11 +0200

    some changes

--- game.js
+++ game.js
@@ -1,4 +1,8 @@
 let cards         = document.querySelectorAll('.cards');
+//_____1_____
+//Une liste avec le nom des animaux -> 6 paires d'animaux. Exemple liste ['girafe', 'girafe', 'zèbre', 'zèbre'] -> A chaque fois deux fois les animaux
+//Prendre les cartes une par une et tirer au sort dans la liste le nom de l'animal inscrit sur la carte puis enlève l'animal de la liste 
+//Ajouter le nom de l'animal tiré au sort et le mettre sur la carte selectionnée (en data-animal ?)
 const ocean         = ['Hyppocampe', 'Hyppocampe', 'Poisson Clown','Poisson Clown', 'Pieuvre', 'Pieuvre', 'Requin', 'Requin', 'Baleine', 'Baleine','Tortue', 'Tortue'];
 const savane        = ['Lion', 'Lion', 'Girafe', 'Girafe', 'Zèbre', 'Zèbre', 'Éléphant', 'Éléphant', 'Chimpanzé', 'Chimpanzé', 'Hippopotame', 'Hippopotame'];
 const ferme         = ['Vache', 'Vache', 'Poule', 'Poule', 'Chien', 'Chien', 'Mouton', 'Mouton', 'Cochon', 'Cochon', 'Cheval', 'Cheval']
@@ -6,10 +10,32 @@ const themes        = [ocean, savane, ferme];
 
 let listCards       = [];
 let counter       = 0;
-//____________1__________Distribution des cartes au tout début :
+//____________2__________Distribution des cartes au tout début :
 CardsDistribution();
 
-//____________2__________Cartes qui se retournent :
+function CardsDistribution() {
+    let theme = SelectTheme();
+    theme     = themes[theme];
+    let cloneTheme   = [...theme];
+    cards.forEach(card => {
+        let index           = Math.floor(Math.random() * (cloneTheme.length));
+        let back            = card.querySelector('.back');
+        back.innerText      = cloneTheme[index];
+        card.dataset.animal = cloneTheme[index];
+        cloneTheme.splice(index, 1);
+    })
+}
+function SelectTheme() {
+    return Math.floor(Math.random() * (themes.length));
+}
+
+//____________3__________Cartes qui se retournent :
+//_____3_____
+//Rentre la carte sélectionnée dans une liste pour qu'il n'y en ai que 2 maximums. 
+//S'il y a moins de 2 cartes dans la liste, alors les cartes sont retournées
+//Inspecte si les deux cartes sont les mêmes, si non, les retournent
+//La carte devient rouge, fait une animation de droite à gauche et se retourne en front
+//Si la carte est la même, la carte devient verte et reste en back et on ne peut plus appuyer dessus
 cards.forEach(card => {
     card.addEventListener('click', (e) => {
         // Prend les cartes
@@ -91,36 +117,14 @@ function addFlippCards(front, back, add){
     back.classList.add('flipper-back-'+add);
 }
 
-function SelectTheme() {
-    return Math.floor(Math.random() * (themes.length));
-}
 
-function CardsDistribution() {
-    let theme = SelectTheme();
-    theme     = themes[theme];
-    let cloneTheme   = [...theme];
-    cards.forEach(card => {
-        let index           = Math.floor(Math.random() * (cloneTheme.length));
-        let back            = card.querySelector('.back');
-        back.innerText      = cloneTheme[index];
-        card.dataset.animal = cloneTheme[index];
-        cloneTheme.splice(index, 1);
-    })
-}
-//_____2_____
-//Une liste avec le nom des animaux -> 6 paires d'animaux. Exemple liste ['girafe', 'girafe', 'zèbre', 'zèbre'] -> A chaque fois deux fois les animaux
-//Prendre les cartes une par une et tirer au sort dans la liste le nom de l'animal inscrit sur la carte puis enlève l'animal de la liste 
-//Ajouter le nom de l'animal tiré au sort et le mettre sur la carte selectionnée (en data-animal ?)
 
-//_____3_____
-//Quand on clique, le nom est affiché. Ajoute 1 au compteur. 
-//Si la deuxième carte tirée n'est pas la même, alors compteur revient à 1. 
-//La carte devient rouge, fait une animation de droite à gauche et se retourne en front
-//Si la carte est la même, la carte devient verte et reste en back et on ne peut plus appuyer dessus
 
 //_____4_____
 //Lancer le timer dès que la première carte est retournée 
 //Si le compteur arrive à 0, c'est perdu, toutes les cartes se retournent et on peut juste appuyer sur rejouer
+
+
 
 //____5_____
 //Si le jeu est gagné, le timer est arrêté, "BRAVO" s'affiche, et on peut juste appuyer sur rejouer
